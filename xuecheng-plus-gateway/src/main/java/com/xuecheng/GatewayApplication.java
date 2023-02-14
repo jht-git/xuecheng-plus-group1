f16415b3e69b296485041f1282ef253f0f2285df
tjhyun <1152584615@qq.com>
2023-02-14 15:24:39 +0800

    搭建网关、并进行网关测试

---
+++ xuecheng-plus-gateway/src/main/java/com/xuecheng/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Tjh
+ * @version 1.0
+ * @description TODO
+ * @date 2023/2/14 14:09
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
